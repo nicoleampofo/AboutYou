d2a6ea3f019400238a66d945c100b77b20e5eb5d
Nicole Ampofo <30731402+nicoleampofo@users.noreply.github.com>
2020-07-25 12:03:31 -0400

    Add files via upload

---
+++ info.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?")
+var lastName = prompt("What is your last name?")
+var age = prompt("What is your current age?")
+var fullName = firstName + " " + lastName;
+alert(fullName + " is " + age + " years old.");
